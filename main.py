9e51d7cff48cf3b87ce567cfe58d65360a96c292
happy <besidefwr@gmail.com>
2023-10-16 18:52:31 +0800

    bug fix

--- main.py
+++ main.py
@@ -80,7 +80,10 @@ def showcontent(username, groupname, isAdmin):
             return render_template('showcontent.html', username=username, groupname=groupname, dbdata=dbdata, isAdmin = isAdmin, posts=posts)
         elif 'deletepost' in request.form:
             form_data = request.form
-            ssf.deleteposts(form_data['groupname'], form_data['username'] ,form_data['postcontent'])
+            if groupname != form_data['groupname']:
+                return render_template('showcontent.html', username=username, groupname=groupname, dbdata=dbdata, isAdmin = isAdmin, posts=posts, state="Fail")
+            else:
+                ssf.deleteposts(form_data['groupname'], form_data['username'] ,form_data['postcontent'])
             posts = ssf.getposts()
             return render_template('showcontent.html', username=username, groupname=groupname, dbdata=dbdata, isAdmin = isAdmin, posts=posts)
         elif 'showfullpost' in request.form:
@@ -122,12 +125,15 @@ def showallpost(groupname: str, username: str, postcontent: str, cgroupname: str
             return render_template('PostnComment.html',groupname=groupname, username=username, postcontent=postcontent, cgroupname=cgroupname, cusername=cusername, all_comments=all_comments, isAdmin=isAdmin)
         elif 'deletecomment' in request.form:
             form_data = request.form
-            ssf.deletecomment(form_data['postcontent'], form_data['groupname'],form_data['username'],form_data['comment'])
+            if form_data['groupname'] != cgroupname:
+                return render_template('PostnComment.html',groupname=groupname, username=username, postcontent=postcontent, cgroupname=cgroupname, cusername=cusername, all_comments=all_comments, isAdmin=isAdmin, state="Fail")        
+            else:      
+                ssf.deletecomment(form_data['postcontent'], form_data['groupname'],form_data['username'],form_data['comment'])
             all_comments = ssf.getallcomment(postcontent)
             return render_template('PostnComment.html',groupname=groupname, username=username, postcontent=postcontent, cgroupname=cgroupname, cusername=cusername, all_comments=all_comments, isAdmin=isAdmin)
         elif 'backtoall' in request.form:
             form_data = request.form
-            return redirect(url_for('showcontent', groupname=groupname, username=username, isAdmin=isAdmin))
+            return redirect(url_for('showcontent', groupname=cgroupname, username=cusername, isAdmin=isAdmin))
     return render_template('PostnComment.html',groupname=groupname, username=username, postcontent=postcontent, cgroupname=cgroupname, cusername=cusername, all_comments=all_comments, isAdmin=isAdmin)
 
 if __name__ == '__main__':
